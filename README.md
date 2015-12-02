5706841d54c336db84fc1da20b99e997d6bf908f
sachdevs <sachdev.96@gmail.com>
2015-12-01 21:54:27 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SoybeanCorrelation
+How to run:
+```
+$ python main.py [correlate/soybean/soybeanoil]
+```
+#####correlate
+Prints pearson correlation coefficient for soybean daily returns vs soybean oil daily returns. Value is around 0.62 which is a strong linear relation (as expected since these commodities are complements in production).
+
+#####soybean/soybeanoil
+Graphs soybean/ soybean oil future daily returns (cumulative) and Long/Short ratios.
+
+##Comments
+The daily returns are calculated a slightly simplified method, should be (at time t) (Last(t) - Last(t-1)) / Last(t-1) <br>
+Done as coding challenge for [Alpha Parity LLC](http://www.alphaparity.com).
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import graph
 import pullquandl
+import sys
 
 # Debugging function to print pandas obj
 def print_full(x):
@@ -11,12 +12,34 @@ def print_full(x):
     print(x)
     pd.reset_option('display.max_rows')
 
-soyF_df = pullquandl.pullSoybeanFutures()
-soyCTR_df = pullquandl.pullSoybeanCTR() 
-graph.draw(soyF_df, 'Total Long Short Ratio Of Soybean Future since 2006', 'Total Longs/Total Shorts', \
-	soyCTR_df, 'Cumulative Daily Returns For Soybean since 1959', 'Cumulative Daily Returns For Soybean')
+def plotSoybeanData():
+	soyF_df = pullquandl.pullSoybeanFutures()
+	soyCTR_df = pullquandl.pullSoybeanCTR() 
+	graph.draw(soyF_df, 'Total Long Short Ratio Of Soybean Future since 2006', 'Total Longs/Total Shorts', \
+		soyCTR_df, 'Cumulative Daily Returns For Soybean since 1959', 'Cumulative Daily Returns For Soybean')
 
-soyOilF_df = pullquandl.pullSoybeanOilFutures()
-soyOilCTR_df = pullquandl.pullSoybeanOilCTR()
-graph.draw(soyOilF_df, 'Total Long Short Ratio Of Soybean Oil Future since 2006', 'Total Longs/Total Shorts', \
-	soyOilCTR_df, 'Cumulative Daily Returns For Soybean Oil since 1959', 'Cumulative Daily Returns For Soybean Oil')
+def plotSoybeanOilData():
+	soyOilF_df = pullquandl.pullSoybeanOilFutures()
+	soyOilCTR_df = pullquandl.pullSoybeanOilCTR()
+	graph.draw(soyOilF_df, 'Total Long Short Ratio Of Soybean Oil Future since 2006', 'Total Longs/Total Shorts', \
+		soyOilCTR_df, 'Cumulative Daily Returns For Soybean Oil since 1959', 'Cumulative Daily Returns For Soybean Oil')
+
+# prints 0.621154007969
+# strong linear relationship - expected since complements in production
+def calculateCorrelationCoefficient():
+	soyF_df = pullquandl.pullSoybeanFutures()
+	soyF_df['dailyReturns'] = (soyF_df['Last'] - soyF_df['Open'])/soyF_df['Open']
+	soyOilF_df = pullquandl.pullSoybeanOilFutures()
+	soyOilF_df['dailyReturns'] = (soyOilF_df['Last'] - soyOilF_df['Open'])/soyOilF_df['Open']
+	print(soyF_df['dailyReturns'].corr(soyOilF_df['dailyReturns'], method='pearson'))
+
+if __name__ == '__main__':
+	argIn = sys.argv[1]
+	if(argIn == 'soybean'):
+		plotSoybeanData()
+	elif(argIn == 'soybeanoil'):
+		plotSoybeanOilData()
+	elif(argIn == 'correlate'):
+		calculateCorrelationCoefficient()
+	else:
+		print("Enter valid command line argument")
